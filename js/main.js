1a51bbd01d5f1f8576ce433cb33653b35f373d2f
Hossam Alahmad <hossamahmad1998@gmail.com>
2021-07-26 12:41:54 +0300

    Add Repo Files.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#search-btn").on("click",function(){
+        $("#search-input").addClass("show-input");
+        $("#search-input").focus();
+    }); 
+    
+});
